386061cabf3174ff3547829b6778550ee2d4de95
Ademola Ariya <ariyaademola@gmail.com>
2024-05-02 14:37:45 -0400

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,43 @@
 import Image from "next/image";
 import { PlayerCards } from "../components/player-cards";
 
+export type Player = {
+  id: string;
+  name: string;
+  position: string;
+  shooting: string;
+  passing: string;
+  dribbling: string;
+  defending: string;
+  image? : {
+    src: string;
+    alt: string;
+  }
+};
+
+export const players: Array<Player> = [
+  {
+    id: "1",
+    name: "Ademola",
+    position: "Defender",
+    shooting: "4",
+    defending: "3",
+    dribbling: "2",
+    passing: "2",
+  },
+  {
+    id: "2",
+    name: "Jide Tella",
+    position: "Attcaker",
+    shooting: "4",
+    defending: "2",
+    dribbling: "4",
+    passing: "4",
+  },
+];
+
 export default function Home() {
   return (
-    <PlayerCards />
+    <PlayerCards players={players} />
   );
 }
